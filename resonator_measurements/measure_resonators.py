d7113ced88c14b8121247a24f140c378972522ad
jorger99 <jorge.ramirez@colorado.ed>
2024-07-31 08:07:01 -0600

    had a dupe script with wrong filename

--- resonator_measurements/measure_resonators.py
+++ resonator_measurements/measure_resonators.py
@@ -1,3 +1,5 @@
+# %%
+
  
 """
     measure_resonators.py 
@@ -23,135 +25,169 @@ import matplotlib.pyplot as plt
 import regex as re
 
 import misc_functions as mf
-import helper_load as hl
-import helper_misc as hm
-import helper_fit as hf
+import helper_plot as hp
+import plot_settings
+ 
+from datetime import datetime
+from janis_ctrl import measure_multiple_resonators
 
-# may need to open this file, save it and
-# then close it for settings to apply
-import plot_settings  
+%run ../setup_measurement
 
-# %%
+# %%  establish measurement variables and parameters
 
-# TODO: create metadata file that shares these parameters with all user files in the folder
+# record date & time for measurement
+dstr = datetime.today().strftime(r'%b_%d_%H%M')
+print(f"\n\nCurrent folder timestamp: {dstr}\n\n")
 
-# XXX: Change the sample name
+# use folder name to get device name
 base_dir = os.path.basename(os.getcwd())
 line_num, sample_name = re.split(r"_", base_dir, maxsplit=1)  # use regex to split only at first _
 
-print(f"{line_num}_{sample_name}")
+# use VNA to record center frequencies
+all_fcs = [4.708899, 5.082241, 
+           5.450325, 5.847405, 
+           6.205773, #6.591279, 
+           6.968852]
 
-# %% to try and get rid of stupid style sheet issue
-plt.subplots(1,1)
-plt.plot()
-plt.show()
+# choose which resonators to measure, and spans/delays
+fcs = all_fcs
+# fcs = all_fcs[-1]   
 
-# %%
+spans = [0.5, 0.5, 0.5, 1.0,
+         0.5, 0.5, 0.5]
+delays = [86.61]*len(fcs)
 
-# search_dir = 'best_datasets'        # hand picked datasets
-search_dir = 'data'                   # all datasets
-# search_dir = r'data\Jul_29_1931'    # specific dataset  
+# create strings for the filename frequencies and data directories 
+freq_strs = [f'{fc:.3f}GHz'.replace('.', 'p') for fc in fcs]
+data_dirs = [f'data\\{dstr}\\{sample_name}_{freq_str}' for freq_str in freq_strs]
 
-# use list comprehension to grab all glob items
-alL_folders = [x for x in glob.glob(f"{search_dir}\\*GHz")]
+display(data_dirs)
 
-# if looking through all datasets, filter through 
-if search_dir == 'data':
-    chosen_timestamp = -1
-    all_timestamped_folders = [x for x in glob.glob(f"{search_dir}\\*") if 'all_plots' not in x]
-    chosen_folder = all_timestamped_folders[chosen_timestamp]
-    alL_folders = [x for x in glob.glob(f"{chosen_folder}\\*GHz")]
+# %% estimate time
 
-plot_dir = f"{search_dir}\\all_plots"
-circle_plots_dir = f"{plot_dir}\\circle_plots"
-power_plots_dir = f"{plot_dir}\\power_plots"
-hm.check_and_make_dir(circle_plots_dir)
-hm.check_and_make_dir(power_plots_dir)
+high_powers_floats = np.arange(-40, -54, -7) + 3
+med_powers_floats = np.arange(-54, -69, -7) + 3 
+low_powers_floats = np.arange(-72, -79, -6) + 3
+ultra_low_powers_floats = np.arange(-84, -91, -6)  + 3   
 
-display(alL_folders)
+power_tuple_dict = { 
+                 # tuple name :  (num of power,  measurement duration in seconds)
+                    "HPow"  : (  len(high_powers_floats),      7  ), 
+                    "MPow"  : (   len(med_powers_floats),      52  ),  
+                    "LPow"  : (   len(low_powers_floats),      496  ),
+                    "ULPow" : ( len(ultra_low_powers_floats),  1233  )
+                    }
 
-# %% e just the circle plots and regular data plots
+mf.estimate_resonator_runtime(power_tuple_dict, num_res=len(fcs))
 
-# chosen_idx = [0, 1, 2, 3, 4, 5, 6, 7]
-# chosen_idx = [-1, -2, -3]
-chosen_idx = range(len(alL_folders))
-
-chosen_resonators = [alL_folders[x] for x in chosen_idx]
-
-for resonator_folder_path in chosen_resonators:
-
-    all_datasets, all_dataset_paths = hl.load_files_in_dir(resonator_folder_path, key="*.csv", debug=False)   
+# use assert to make sure every freq has a span & delay
+assert len(fcs) == len(spans), f"length of fcs =/= spans  ({len(fcs)} =/= {len(spans)})"
+assert len(fcs) == len(delays), f"length of fcs =/= delays ({len(fcs)} =/= {len(delays)} )"
     
-    fig, ax = mf.plot_whole_directory(resonator_folder_path, "*\\*.csv", plot_dir=power_plots_dir,
-                                        max_rows=5, verbose=False, plot_min=True, save_plot=False, show_plot=True)
-        
-    mf.plot_all_circles(resonator_folder_path, "*\\*.csv", plot_dir=circle_plots_dir, 
-                                show_line=False, verbose=False, save_plot=False, show_plot=True)
+print(high_powers_floats, med_powers_floats, low_powers_floats, ultra_low_powers_floats)
 
+# %% high power scan
+high_powers = [int(x) for x in high_powers_floats]
 
-                        
- # %%  individual folder loss tan fit 
-
-# perform_loss_tan_fit = False
-perform_loss_tan_fit = True
+HPow_num_avgs, HPow_IFBW_kHz, HPow_num_pts = 3, 1, 301
  
-# chosen_idx = [0, 1, 2, 3, 4, 5, 6,]
-# chosen_idx = [-1]
-chosen_resonators = [alL_folders[x] for x in chosen_idx]
+tStart_high = time.time()   
+measure_multiple_resonators(fcs, spans, delays, high_powers, 
+                            ifbw = HPow_IFBW_kHz, npts = HPow_num_pts,  
+                            sample_name = sample_name, data_dirs = data_dirs, 
+                            Nf = int(HPow_num_pts*0.8), Navg_init = HPow_num_avgs,  # Nf = homosphasal fractions
+                            Noffres = int(HPow_num_pts*0.2),  offresfraction = 0.1, # variables for segmented
+                            filename_suffix="", verbose = False, wait_time = 3, 
+                            segment_option="segmented",
+                            # segment_option="linear",
+                            # segment_option="homophasal",
+                            # segment_option="hybrid",
+                            )
+tEnd_high = time.time()
 
-for resonator_folder_path in chosen_resonators:
-    
-    all_names, all_paths = hl.load_files_in_dir(resonator_folder_path, key="*.csv", blacklist='qiqc', debug=False)   
 
-    powers_in = [hm.get_power_from_filename(x) for x in all_names if 'dB' in x] 
+mf.print_text_block(tStart_high, tEnd_high, HPow_num_avgs, HPow_num_pts, HPow_IFBW_kHz, num_powers=len(high_powers), num_resonators=len(fcs))
 
-    # all should be within 10 mK anyway
-    all_temperatures =  [int(hm.get_temperature_from_filename(fname)) for fname in all_names if 'qiqc' not in fname] 
-    temperature = int(np.ceil(np.average(all_temperatures)))  
-    
-    # placeholder
-    init_conds = [None]*len(all_names)
-    # init_conds = [[5e5, 5e5, 0, np.pi/2] ]
+for res_folder in data_dirs: 
+    fig, ax = mf.plot_whole_directory(res_folder, "*\\*.csv", plot_dir=res_folder, 
+                                        max_rows=5, verbose=False, plot_min=True, save_plot=True, show_plot=True)
+        
 
-    all_fits_save_dir = f"{resonator_folder_path}\\dcm_fits\\"
-    qiqc_fit_save_dir = f"{plot_dir}"
 
-    save_fit_dirs = [all_fits_save_dir, qiqc_fit_save_dir]
-    for fit_dir in save_fit_dirs:
-        hm.check_and_make_dir(fit_dir)
-    
-    try:
-        hf.power_sweep_fit_drv(sample_name=sample_name,
-                            atten=[0, -70], temperature=temperature,
-                            powers_in=powers_in, all_paths=all_paths, 
-                            # plot_from_file=False,
-                            use_error_bars=True, temp_correction='', phi0=0.,
-                            use_gauss_filt=False, use_matched_filt=False,
-                            use_elliptic_filt=False, use_mov_avg_filt=False,
-                            loss_scale=1e-6, preprocess_method='linear',
-                            ds = {'QHP' : 1e5, 'nc' : 1e1, 'Fdtls' : 1e-6},
-                            plot_twinx=False, plot_fit=perform_loss_tan_fit, QHP_fix=True, show_plots=True,
-                            data_dir=resonator_folder_path, save_dcm_plot=True, manual_init_list=init_conds,
-                            save_fit_dirs=save_fit_dirs, show_dbm=True)
-        plt.show()
-    except Exception as e:
-        display(resonator_folder_path)
-        print( "====================================================================================")
-        print( "====================================================================================")
-        print(f"=======================    Failed to fit:  {sample_name}    ========================")
-        print( "====================================================================================")
-        print( "====================================================================================")
-        print("Error Message:  \n")
-        print(e)   # TODO: add stack trace  
-        print("\n\n")
-            
-# %% plot whole directory
+# %% medium power scan
+med_powers = [int(x) for x in med_powers_floats]
 
-# best_datasets_path = 'best_datasets'
-# best_resonator_paths = glob.glob(f"{best_datasets_path}\\*GHz")
+MPow_num_avgs, MPow_IFBW_kHz, MPow_num_pts = 80, 1.0, 301
 
-# for resonator_path in best_resonator_paths:
-#     plot_dir_path = f"{resonator_path}"
-#     mf.plot_whole_directory(resonator_path, search_str="\\*.csv", plot_dir=plot_dir_path, 
-#                             save_plot=True, show_plot=False, verbose=True)
+tStart_med = time.time()   
+measure_multiple_resonators(fcs, spans, delays, med_powers, 
+                            ifbw = MPow_IFBW_kHz, npts = MPow_num_pts,  
+                            sample_name = sample_name, data_dirs = data_dirs,
+                            Nf = int(MPow_num_pts*0.8), Navg_init = MPow_num_avgs,  # Nf = homosphasal fractions
+                            Noffres = int(MPow_num_pts*0.2),  offresfraction = 0.1, # variables for segmented
+                            filename_suffix="", verbose = False, wait_time = 3, 
+                            segment_option="segmented",
+                            # segment_option="linear",
+                            # segment_option="homophasal",
+                            # segment_option="hybrid",
+                            )
+tEnd_med = time.time()
 
+for res_folder in data_dirs: 
+    fig, ax = mf.plot_whole_directory(res_folder, "*\\*.csv", plot_dir=res_folder, 
+                                        max_rows=5, verbose=False, plot_min=True, save_plot=True, show_plot=True)
+        
+
+mf.print_text_block(tStart_med, tEnd_med, MPow_num_avgs, MPow_num_pts, MPow_IFBW_kHz, num_powers=len(med_powers), num_resonators=len(fcs))
+
+
+# %% low power scan
+low_powers = [int(x) for x in low_powers_floats]
+
+LPow_num_avgs, LPow_IFBW_kHz, LPow_num_pts = 2400, 1.0, 101
+
+tStart_low = time.time()   
+measure_multiple_resonators(fcs, spans, delays, low_powers,
+                            ifbw = LPow_IFBW_kHz, npts = LPow_num_pts,  
+                            sample_name = sample_name, data_dirs = data_dirs,
+                            Nf = int(LPow_num_pts*0.8), Navg_init = LPow_num_avgs,  # Nf = homosphasal fractions
+                            Noffres = int(LPow_num_pts*0.2),  offresfraction = 0.1, # variables for segmented
+                            filename_suffix="", verbose = False, wait_time = 3, 
+                            segment_option="segmented",
+                            # segment_option="linear",
+                            # segment_option="homophasal",
+                            # segment_option="hybrid",
+                            )
+tEnd_low = time.time()
+
+for res_folder in data_dirs: 
+    fig, ax = mf.plot_whole_directory(res_folder, "*\\*.csv", plot_dir=res_folder, 
+                                        max_rows=5, verbose=False, plot_min=True, save_plot=True, show_plot=True)
+
+mf.print_text_block(tStart_low, tEnd_low, LPow_num_avgs, LPow_num_pts, LPow_IFBW_kHz, num_powers=len(low_powers), num_resonators=len(fcs))
+
+
+# %% ultra low power scan
+ultra_low_powers = [int(x) for x in ultra_low_powers_floats]
+
+ULPow_num_avgs, ULPow_IFBW_kHz, ULPow_num_pts = 6000, 1.0, 101
+
+tStart_ultra_low = time.time()   
+measure_multiple_resonators(fcs, spans, delays, ultra_low_powers,
+                            ifbw = ULPow_IFBW_kHz, npts = ULPow_num_pts,  
+                            sample_name = sample_name, data_dirs = data_dirs,
+                            Nf = int(ULPow_num_pts*0.8), Navg_init = ULPow_num_avgs,  # Nf = homosphasal fractions
+                            Noffres = int(ULPow_num_pts*0.2),  offresfraction = 0.1, # variables for segmented
+                            filename_suffix="", verbose = False, wait_time = 3, 
+                            segment_option="segmented",
+                            # segment_option="linear",
+                            # segment_option="homophasal",
+                            # segment_option="hybrid",
+                            )
+tEnd_ultra_low = time.time()
+
+for res_folder in data_dirs: 
+    fig, ax = mf.plot_whole_directory(res_folder, "*\\*.csv", plot_dir=res_folder, 
+                                        max_rows=5, verbose=False, plot_min=True, save_plot=True, show_plot=True)
+
+mf.print_text_block(tStart_ultra_low, tEnd_ultra_low, ULPow_num_avgs, ULPow_num_pts, ULPow_IFBW_kHz, num_powers=len(ultra_low_powers), num_resonators=len(fcs))
