74bf6ce06c83240cf011daca14971faffaa649c5
jorger99 <jorge.ramirez@colorado.edu>
2024-11-05 18:11:19 -0700

    re-organized helper methods into 'Reset', 'Take', and 'Archive' data to reflect the characterization process. Also added a small example at the bottom, unfinished

--- prototype_msmt_code/experiments/VNA Experiments/VNA Characterization/TakeMeasurements.py
+++ prototype_msmt_code/experiments/VNA Experiments/VNA Characterization/TakeMeasurements.py
@@ -66,12 +66,42 @@ DefaultConfig = {
 
 # %%
 
-def SetupMeasurement(VNA, data_dir=None):
+""" 
+    most of the measurements for the characterization will be taken in the interactive window, so
+        let's create some helper functions to speed up that process
+        
+    characterization workflow:
+    
+        1) Reset_VNA() will set the default configs and then call setup_s2p_measurement() to get 
+            all four s-parameters ready for measurement
+        2) Take_Data() will check for errors, perform measurement, and then plot for quick visualization
+        3) Repeat steps 1-2 until happy with trace
+             .
+             .
+        4) Archive_Results() will get the results from the VNA and save to a specified location with
+            appropriate metadata.
+
+"""
+
+def Reset_VNA(VNA, **kwargs):
+    """
+        Resets the VNA to default configs, then passes any kwargs to the configs,
+        and then sets up the s2p measurement.
+    """
     VNA.set_instr_params(DefaultConfig)
     VNA.get_instr_params()
+    VNA.add_kwargs_and_filter_configs(**kwargs)
     VNA.setup_s2p_measurement()
     
-def TakeData(VNA):
+    
+def Take_Data(VNA):
+    """
+        instruct VNA to acquire data and return it in freqs/magn/phase
+        
+        returns:
+            df = pandas dataframe of acquired data
+                 eight columns of magn/phase scattering parameters
+    """
     
     VNA.check_instr_error_queue()
     VNA.acquire_trace()
@@ -79,37 +109,60 @@ def TakeData(VNA):
     freqs, magn_dB, phase_deg = VNA.return_data()
     
     #########################
-    
+    # plot data with helper function
     df, fig, axes = qh.plot_data_with_pandas(freqs, magn_dB, phase_deg=phase_deg)
 
-    title_str = str(f"{Expt_Config["span"]/1e6:1.2f}MHz_span_{Expt_Config["averages"]}_avgs_{Expt_Config["if_bandwidth"]}_IFBW_{Expt_Config["power"]}_dBm")
+    # add datetime to first row for archive
+    first_row = {col : val for col, val in zip(df.cols, [datetime.datetime()]*len(df.cols))}
+    datetime_row = pd.DataFrame(first_row, index=["datetime.now()"])
+    df = pd.concat([datetime_row, df.iloc[:]])
+    
+    title_str = str(f"{VNA.config["f_span"]/1e6:1.2f}MHz_span_{VNA.config["averages"]}_avgs_{VNA.config["if_bandwidth"]}_IFBW_{VNA.config["power"]}_dBm")
     fig = axes["A"].get_figure()
     fig.suptitle(title_str, size=16)
     fig.tight_layout()
     
     # fc, span, ifbw, avg, power = Expt_Config["fc"], Expt_Config["span"], Expt_Config["if_bandwidth"], Expt_Config["averages"], Expt_Config["power"]
 
+    return df
     #########################
+
+def Archive_Data(VNA, df, experiment_name, save_dir="./data"):
+    # check if save_dir is a path or string
+    if not isinstance(save_dir, Path):
+        save_dir = Path(save_dir)
     
-    expt_no = len(data_dir.glob("*.csv")) + 1    # append number to end of filename
-    filename = str(data_dir / f"{experiment_name}_{expt_no:03d}.csv")
+    # check if save_dir exists
+    if not save_dir.exists():
+        VNA.print_console(f"Creating directory {save_dir} for {experiment_name}")
+        save_dir.mkdir(exist_ok=True, parents=True)
     
-    # want to add datetime to file, but must match dimensions, so just add it as first row... oops :)
+    # append number to end of filename and save to csv
+    expt_no = len(save_dir.glob("*.csv")) + 1    
+    filename = str(save_dir / f"{experiment_name}_{expt_no:03d}.csv")
+    df.to_csv(filename)
     
-    first_row = {col : val for col, val in zip(df.cols, [datetime.datetime()]*len(df.cols))}
-    datetime_row = pd.DataFrame(first_row, index=["datetime.now()"])
-    df2 = pd.concat([datetime_row, df.iloc[:]])
-    df2.to_csv(filename)
+    return df
 
     #########################
 
-def Get_Data(VNA):
+# %% example usage
+
+Measurement_Configs = {
+    "f_start" : 2e9,
+    "f_stop" : 10e9,
+    "n_pts" : 2001,
+    "if_bw" : 5000,
+    "power" : -20,
     
-    
-    
-def Save_Measurement(VNA, experiment_name, csv_path, ):
-    if not isinstance(data_dir, Path):
-        data_dir = Path(data_dir)
-    
-    return 
-#% %
+    # by default, sparam = 'all', edelay = 0, averages = 10
+}
+
+expt_name = "Test_Measurement"
+
+Reset_VNA(PNA_X, **Measurement_Configs)
+Take_Data(PNA_X)
+
+
+
+# %%
