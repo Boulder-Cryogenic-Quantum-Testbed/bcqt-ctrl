b602fe3e1f2faeb43663d308e637e8367d2d8a39
jorger99 <jorge.ramirez@colorado.edu>
2024-11-14 16:13:44 -0700

    changed name of measurement script. removed the mini-methods to have more control

--- prototype_msmt_code/experiments/VNA Experiments/VNA Characterization/TakeMeasurements.py
+++ prototype_msmt_code/experiments/VNA Experiments/VNA Characterization/TakeMeasurements.py
@@ -3,8 +3,8 @@
     Test implementation of VNA driver
 """
 
-%load_ext autoreload
-%autoreload 2
+# %load_ext autoreload
+# %autoreload 2
 
 # %%
 
@@ -47,9 +47,23 @@ VNA_Keysight_InstrConfig = {
     # "rm_backend" : "@py",
     "rm_backend" : None,
     "instr_address" : 'TCPIP0::192.168.0.105::inst0::INSTR',
+    "n_points" : 1001,
+    "f_start" : 2e9,
+    "f_stop" : 10e9,
+    "if_bandwidth" : 1000,
+    "power" : -50,
+    "edelay" : 0,
+    "averages" : 2,
+    # "sparam" : ['S12'],  # do not want to measure S12 in VNA two port mode
+    "sparam" : ['S11', 'S22', 'S21', 'S12'],  # do not want to measure S12 in VNA two port mode
+    # "sparam" : ['S12', 'S21'],  # do not want to measure S12 in VNA two port mode
+    
+    "segment_type" : "linear",
 }
 
 PNA_X = VNA_Keysight(VNA_Keysight_InstrConfig, debug=True)
+
+PNA_X.set_instr_params(VNA_Keysight_InstrConfig)
 
 # %%
 
@@ -69,48 +83,14 @@ PNA_X = VNA_Keysight(VNA_Keysight_InstrConfig, debug=True)
             appropriate metadata.
 """
 
-def Reset_VNA(VNA, config, **kwargs):
-    """
-        Resets the VNA to default configs, then passes any kwargs to the configs,
-        and then sets up the s2p measurement.
-    """
-    VNA.check_instr_error_queue()
-    VNA.set_instr_params(config)
-    VNA.get_instr_params()
-    
-    
-def Make_Measurement(VNA, **kwargs):
-    """
-        instruct VNA to do error checks, add configs, and perform measurement
-    """
-    VNA.setup_s2p_measurement()
-    VNA.add_kwargs_and_filter_configs(**kwargs)
-    VNA.check_instr_error_queue()
-    VNA.run_measurement()
-    
-def Acquire_Trace(VNA, plot_complex=True, track_min=True, title="", do_edelay_fit=True):
-    """
-        Should take zero time, only asks VNA to send the data it has
-        
-        
-        returns:
-            df = pandas dataframe of acquired data
-                 eight columns of magn/phase scattering parameters
-    
-    """
-    df = VNA.return_data_s2p()
-    all_axes = qh.plot_s2p_df(df, plot_complex, track_min, title, do_edelay_fit=do_edelay_fit)
-    
-    return df, all_axes
-        #########################
-
 def Archive_Data(VNA, s2p_df:pd.DataFrame, meas_name:str, expt_category:str = '', save_dir:str = "./data", all_axes=None):
     # check if save_dir is a path or string
     if not isinstance(save_dir, Path):
         save_dir = Path(save_dir).absolute()
         
     timestamp = datetime.today().strftime("%m_%d_%I%M%p")
-    file_dir = save_dir / expt_category / meas_name / timestamp
+    # file_dir = save_dir / expt_category / meas_name / timestamp
+    file_dir = save_dir / expt_category / meas_name
     
     # check if file_dir exists
     if not file_dir.exists():
@@ -119,7 +99,7 @@ def Archive_Data(VNA, s2p_df:pd.DataFrame, meas_name:str, expt_category:str = ''
         file_dir.mkdir(exist_ok=True, parents=True)
     
     # append number to end of filename and save to csv
-    expt_no = len(list(save_dir.glob("*.csv"))) + 1    
+    expt_no = len(list(file_dir.glob("*.csv"))) + 1    
     filename = f"{meas_name}_{expt_no:03d}.csv"
     VNA.print_console(f"Saving data as {filename}")
     VNA.print_console(f"    under '{str(Path(*file_dir.parts[-6:]))}'")
@@ -132,7 +112,7 @@ def Archive_Data(VNA, s2p_df:pd.DataFrame, meas_name:str, expt_category:str = ''
         for axes in all_axes:
             ax = axes[0]
             fig = ax.get_figure()
-            title = fig.get_suptitle().replace(" - ","_") + ".png"
+            title = fig.get_suptitle().replace(" - ","_") + f"{expt_no:03d}.png"
             fig_filename = file_dir / title
             fig.tight_layout()
             fig.savefig(fig_filename)
@@ -142,48 +122,93 @@ def Archive_Data(VNA, s2p_df:pd.DataFrame, meas_name:str, expt_category:str = ''
     
     return filename, final_path.parent
 
-
-    #########################
-# %% set default values
-
-DefaultConfig = {
-    "n_points" : 20001,
-    "f_start" : 4e9,
-    "f_stop" : 8e9,
-    "if_bandwidth" : 5000,
-    "power" : -75,
-    "edelay" : 0,
-    "averages" : 3,
-    "sparam" : ['S11', 'S22', 'S21'],  # do not want to measure S12 in VNA two port mode
+def Load_CSV(filepath):
+    """
+        convenience method - take csv filepath and return df, magns, phases, and freqs
+    """
     
-    "segment_type" : "linear",
-}
+    if not isinstance(filepath, Path):
+        filepath = Path(filepath).absolute()
+    
+    df = pd.read_csv(filepath, index_col=0)
+    
+    all_magns, all_phases = [], []
+    
+    for col in df:
+        if "magn" in col.lower():
+            series = df[col].values[1:]
+            all_magns.append(series)
+        if "phase" in col.lower():
+            series = df[col].values[1:]
+            all_phases.append(series)
+        if "freq" in col.lower():
+            freqs = df[col].values[1:]
+    
+    return df, all_magns, all_phases, freqs
 
-PNA_X.set_instr_params(DefaultConfig)
-
-# %% example usage
+    
+    #########################
+# %%
 
 Measurement_Configs = {
     "f_start" : 2e9,
     "f_stop" : 10e9,
-    "n_pts" : 4001,
-    "if_bw" : 5000,
-    "power" : -60,
-    "averages" : 3,
+    "n_pts" : 10001,
+    "if_bw" : 1000,
+    "power" : -50,
+    "averages" : 2,
+    "sparam" : ['S11', 'S22', 'S21', 'S12'], 
     
     # by default, sparam = 'all', edelay = 0, averages = 10
 }
 
-expt_category = "Circulators"
-meas_name = "VNA_Purple_Cables"
 
-Reset_VNA(PNA_X, Measurement_Configs)
-Make_Measurement(PNA_X)
 
-# %%
-# TODO: stop this method from resetting display configs
-all_dfs, all_axes = Acquire_Trace(PNA_X, plot_complex=False, track_min=False, title=meas_name, do_edelay_fit=True)
-filename, filepath = Archive_Data(PNA_X, all_dfs, meas_name=meas_name, expt_category=expt_category, all_axes=all_axes)
+# PNA_X.check_instr_error_queue()
+# PNA_X.add_kwargs_and_filter_configs()
 
-# %%
+"""
+    ResetVNA
+        Resets the VNA to default configs, then passes any kwargs to the configs,
+        and then sets up the s2p measurement.
+"""
+PNA_X.set_instr_params(Measurement_Configs)
+PNA_X.get_instr_params()
 
+
+"""
+    PerformMeasurement
+        instruct VNA to do error checks, add configs, and perform measurement
+"""
+PNA_X.setup_s2p_measurement()
+PNA_X.run_measurement()
+
+
+"""
+    DownloadMeasurement
+    Should take zero time, only asks VNA to send the data it has, does not measure
+        df = dataframe of scattering parameters
+
+"""
+s2p_df = PNA_X.return_data_s2p()
+all_axes = qh.plot_s2p_df(s2p_df)
+
+"""
+    Archive_Data(VNA, s2p_df, meas_name, expt_category, save_dir="./data", all_axes=None):
+    return filename, final_path.parent
+"""
+
+expt_category = "HEMT_RT_Amplifiers"
+meas_name = "HEMT_B"
+
+filename, filepath = Archive_Data(PNA_X, s2p_df, meas_name=meas_name, expt_category=expt_category, all_axes=all_axes)
+
+
+
+# %% manually load data
+
+load_filepath = r"C:\Users\Lehnert Lab\GitHub\bcqt-ctrl\prototype_msmt_code\experiments\VNA Experiments\VNA Characterization\data_jin_characterization\S12_MXC\001_Input_Line24_to_Attenuators\Input_Line24_to_Attenuators_001.csv"
+
+loaded_df, all_magns, all_phases, freqs = Load_CSV(load_filepath)
+
+all_axes = qh.plot_s2p_df(loaded_df, plot_complex=True, track_min=False, title=meas_name, do_edelay_fit=False)
